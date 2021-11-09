54f46774de0ec390371e3f1bb6d07a60df3366fd
q876625596 <876625596@qq.com>
2021-11-09 22:07:13 +0800

    add size

--- lib/responsive_sizer.dart
+++ lib/responsive_sizer.dart
@@ -3,6 +3,8 @@ library responsive_sizer;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
 import 'package:flutter/widgets.dart';
 
+part 'src/extension.dart';
+
 part 'src/helper.dart';
 
 part 'src/widget.dart';
